d94388446fe3876e350fecf9714561153d030cea
Jean GIESE <jean.giese@etu.unistra.fr>
2025-09-19 14:47:54 +0200

    Ajout d'un menu prototype + rangement des fichiers

---
+++ Scripts/menu.gd
@@ -0,0 +1,14 @@
+extends Panel
+
+
+
+func _on_facile_pressed():
+	pass # Replace with function body.
+
+
+func _on_moyen_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_difficile_pressed() -> void:
+	pass # Replace with function body.
