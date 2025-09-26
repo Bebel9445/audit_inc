5e364c0fdf5ce65adfc517121898f79ba08cd5ca
Wilson <wilson.adjei@etu.unistra.fr>
2025-09-26 14:48:04 +0200

    Ajout de la logique pour la difficulte

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -3,14 +3,16 @@ extends Node
 @export var carte_scene : PackedScene
 
 var choixPoles_instance
+var current_difficulte : int
 
 func _ready():
 	pass
 	
 
-func new_game():
+func new_game(difficulte : int):
+	print(difficulte)
+	current_difficulte = difficulte
 	$Menu.queue_free()
-	
 	choixPoles_instance = choixPoles_scene.instantiate()
 	add_child(choixPoles_instance)	
 	choixPoles_instance.poleEconomie.connect(lancer_audit)
