962bfacd8ec81ec904c259200e18bbf87fde5ecc
Jean GIESE <jean.giese@etu.unistra.fr>
2025-10-10 14:29:03 +0200

    boutons des cartes connecter au main

--- Scripts/initCarte.gd
+++ Scripts/initCarte.gd
@@ -1,5 +1,8 @@
 extends MarginContainer
 
+signal choixGauche
+signal choixDroite
+
 @onready var card_image: TextureRect = $VBoxContainer/TextureRect
 @onready var card_text: Label = $VBoxContainer/Label
 @onready var card_text_gauche: Button = $VBoxContainer/HBoxContainer/BoutonGauche
@@ -13,3 +16,9 @@ func set_card(image: Texture2D, text: String, textGauche: String, textDroite: St
 	card_text.text = text
 	card_text_gauche.text = textGauche
 	card_text_droite.text = textDroite
+
+func _on_bouton_gauche_pressed():
+	choixGauche.emit("Gauche")
+
+func _on_bouton_droite_pressed():
+	choixDroite.emit("Droite")
